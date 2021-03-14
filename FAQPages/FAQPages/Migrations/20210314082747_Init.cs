a764455faa400ba2dce10f5f6fc98c615e1c94b1
Mondi Koci <101282062@georgebrown.ca>
2021-03-14 04:29:38 -0400

    Initial Migration
    - Models
    - EF Configs
    - Routings
    - Initial Migrations

---
+++ FAQPages/FAQPages/Migrations/20210314082747_Init.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FAQPages.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Topics",
+                columns: table => new
+                {
+                    TopicId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topics", x => x.TopicId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FAQs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TopicId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CategoryId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FAQs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FAQs_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FAQs_Topics_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topics",
+                        principalColumn: "TopicId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { "gen", "General" },
+                    { "hist", "History" },
+                    { "tech", "Technology" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Topics",
+                columns: new[] { "TopicId", "Name" },
+                values: new object[,]
+                {
+                    { "boot", "Bootstrap" },
+                    { "cs", "C#" },
+                    { "js", "JavaScript" },
+                    { "academia", "Academia" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "FAQs",
+                columns: new[] { "Id", "Answer", "CategoryId", "Question", "TopicId" },
+                values: new object[,]
+                {
+                    { 1, "In 2011", "hist", "When was Bootstrap released", "boot" },
+                    { 7, "A CSS framwork for creating responsive web applications for multiple screen sizes", "gen", "What is Bootstrap?", "boot" },
+                    { 3, "In 2002", "hist", "When was C# first released?", "cs" },
+                    { 6, "A general purpose object-oriented language that uses concise java-like syntax", "gen", "What is C#?", "cs" },
+                    { 4, "In 1995", "hist", "When was JavaScript first released?", "js" },
+                    { 5, "JavaScript is a general purpose scripting language that executes in the browser", "gen", "What is JavaScript", "js" },
+                    { 8, "A general purpose scripting language that executes in a web browser", "gen", "What is JavaScript?", "js" },
+                    { 2, "In 387 BC, Classical Athens", "hist", "When did Plato found the Academy?", "academia" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FAQs_CategoryId",
+                table: "FAQs",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FAQs_TopicId",
+                table: "FAQs",
+                column: "TopicId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FAQs");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Topics");
+        }
+    }
+}
---
+++ FAQPages/FAQPages/Models/FAQsContext.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FAQPages.Models
+{
+    public class FaqsContext : DbContext
+    {
+        public FaqsContext(DbContextOptions<FaqsContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<FAQ> FAQs { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                    new Category { CategoryId = "gen", Name = "General" },
+                    new Category { CategoryId = "hist", Name = "History" },
+                    new Category { CategoryId = "tech", Name = "Technology" }
+                );
+            modelBuilder.Entity<Topic>().HasData(
+                    new Topic { TopicId = "boot", Name = "Bootstrap" },
+                    new Topic { TopicId = "cs", Name = "C#" },
+                    new Topic { TopicId = "js", Name = "JavaScript" },
+                    new Topic { TopicId = "academia", Name = "Academia" }
+                );
+            modelBuilder.Entity<FAQ>().HasData(
+                    new FAQ
+                    {
+                        Id = 1,
+                        Question = "When was Bootstrap released",
+                        Answer = "In 2011",
+                        TopicId = "boot",
+                        CategoryId = "hist"
+                    },
+                    new FAQ
+                    {
+                        Id = 2,
+                        Question = "When did Plato found the Academy?",
+                        Answer = "In 387 BC, Classical Athens",
+                        TopicId = "academia",
+                        CategoryId = "hist"
+                    },
+                    new FAQ
+                    {
+                        Id = 3,
+                        Question = "When was C# first released?",
+                        Answer = "In 2002",
+                        TopicId = "cs",
+                        CategoryId = "hist"
+                    },
+                    new FAQ
+                    {
+                        Id = 4,
+                        Question = "When was JavaScript first released?",
+                        Answer = "In 1995",
+                        TopicId = "js",
+                        CategoryId = "hist"
+                    },
+                    new FAQ
+                    {
+                        Id = 5,
+                        Question = "What is JavaScript",
+                        Answer = "JavaScript is a general purpose scripting language that executes in the browser",
+                        TopicId = "js",
+                        CategoryId = "gen"
+                    },
+                    new FAQ
+                    {
+                        Id = 6,
+                        Question = "What is C#?",
+                        Answer = "A general purpose object-oriented language that uses concise java-like syntax",
+                        TopicId = "cs",
+                        CategoryId = "gen"
+
+                    },
+                    new FAQ
+                    {
+                        Id = 7,
+                        Question = "What is Bootstrap?",
+                        Answer = "A CSS framwork for creating responsive web applications for multiple screen sizes",
+                        TopicId = "boot",
+                        CategoryId = "gen"
+
+                    },
+                    new FAQ
+                    {
+                        Id = 8,
+                        Question = "What is JavaScript?",
+                        Answer = "A general purpose scripting language that executes in a web browser",
+                        TopicId = "js",
+                        CategoryId = "gen"
+
+                    }
+                );
+        }
+
+    }
+}
