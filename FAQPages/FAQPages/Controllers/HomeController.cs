cebfd6a401be983ec03362b2400eca3736ab48f9
Mondi Koci <101282062@georgebrown.ca>
2021-03-14 06:10:36 -0400

    Basic data process and presentation

--- FAQPages/FAQPages/Controllers/HomeController.cs
+++ FAQPages/FAQPages/Controllers/HomeController.cs
@@ -1,20 +1,40 @@
 ﻿using FAQPages.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace FAQPages.Controllers
 {
     public class HomeController : Controller
     {
-
-        public IActionResult Index()
+        public FaqsContext context { get; set; }
+        public HomeController(FaqsContext ctx) 
         {
-            return View();
+            context = ctx;
+        }
+        public IActionResult Index(string topic, string category)
+        {
+            ViewBag.Topics = context.Topics.OrderBy(t => t.Name).ToList();
+            ViewBag.Categories = context.Categories.OrderBy(c => c.Name).ToList();
+            ViewBag.SelectedTopic = topic;
+
+            IQueryable<FAQ> faqs = context.FAQs
+                       .Include(f => f.Topic)
+                       .Include(f => f.Category)
+                       .OrderBy(f => f.Question);
+
+            //Where clauses if topic is passed in
+            if (!string.IsNullOrEmpty(topic)) {
+                faqs = faqs.Where(f => f.TopicId == topic);
+            }
+
+            if (!string.IsNullOrEmpty(category))
+            {
+                faqs = faqs.Where(f => f.CategoryId == category);
+            }
+
+
+            return View(faqs.ToList());
         }
 
     }
